8d2be2746cf1f51f8e033950e477bfec6b37c36d
Adam Malej <adammalej1@gmail.com>
2018-11-13 21:59:20 +0100

    Add configurtion about cors and cookies

--- SimpleFacebookBackend/Startup.cs
+++ SimpleFacebookBackend/Startup.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 
 namespace SimpleFacebookBackend
 {
@@ -25,7 +28,40 @@ namespace SimpleFacebookBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                var keyByte = Encoding.UTF8.GetBytes("MySupersecurityKey");
+                var signingKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(keyByte);
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = "http://localhost:50882",
+                    ValidIssuer = "http://localhost:50882",
+                    IssuerSigningKey = signingKey
+                };
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //Connection when database will be on server
+            //var connection = @"Server=(localdb)\mssqllocaldb;Database=StronaInternetowa.Context;Trusted_Connection=True;ConnectRetryCount=0";
+            //services.AddDbContext<StronaInternetowaContext>
+            //    (options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
